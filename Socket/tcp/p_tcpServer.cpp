783460238e7f03972eeb647724aa99201513c4ae
Mmmmmmi <812632545@qq.com>
2019-01-19 22:04:02 +0800

    2019.01.19

---
+++ Socket/tcp/p_tcpServer.cpp
@@ -0,0 +1,83 @@
+/*
+ *  这是一个用C++实现的tcp服务端，使用了多进程来保证实现一个服务器对应多个客户端
+ *
+ */
+
+
+#include <sys/wait.h>
+#include "tcpHead.h"
+
+int main(int argc, char* argv[])
+{
+
+    TcpSocket tcpserver;
+    
+    if (argc != 3) {
+        printf("error for input");
+        return -1;
+    }
+    std::string ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    //创建套接字
+    CHECK_RET(tcpserver.creatSocket());
+
+    //绑定套接字
+    CHECK_RET(tcpserver.bindSocket(ip, port));
+
+
+    //监听
+    CHECK_RET(tcpserver.listenSocket(5));
+
+    while(1) {
+
+        int forkret = fork();
+        if (forkret == -1) {
+            perror("error for fork");
+            return -1;
+        }else if (forkret == 0) {
+            //第一层子进程          由于进程无法分离，所以直接创建两个子进程，让第一个子进程直接返回，这样第二个子进程就成了孤儿进程，系统就会回收它
+            int retfork = fork();
+            if (retfork == -1) {
+                perror("error for fork");
+                return -1;
+            }else if (retfork == 0){
+                //第二层子进程
+
+                TcpSocket newsocket;
+
+                //获取新的文件描述符 
+                if (tcpserver.acceptSocket(newsocket) == false) {
+                    continue;
+                }
+
+                char buf[1024] = {0}; 
+                while(1) {
+
+                    //接收
+                    newsocket.receiveSocket(buf, 1023);
+                    printf("Client  Say :  %s\n", buf);
+
+
+                    //发送
+                    printf("Server Say : ");     
+                    fflush(stdout);
+                    memset(buf, 0, 1024);
+                    scanf("%s", buf);
+                    newsocket.sendSocket(buf, strlen(buf));
+                }
+                newsocket.closeSocket();
+                exit(0);
+            }else {
+                exit(0);
+            }
+
+        }else {
+            wait(NULL);
+        }
+    }
+    tcpserver.closeSocket();
+    
+    return 0;
+}
+
---
+++ Socket/tcp/tcpServer.cpp
@@ -0,0 +1,67 @@
+/*
+ *  这是一个用C++实现的tcp服务端，目前只能对应一个客户端
+ *
+ *
+ */
+
+
+
+
+
+#include "tcpHead.h"
+
+int main(int argc, char* argv[])
+{
+
+    TcpSocket tcpserver;
+    
+    if (argc != 3) {
+        printf("error for input");
+        return -1;
+    }
+    std::string ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    //创建套接字
+    CHECK_RET(tcpserver.creatSocket());
+
+
+    //绑定套接字
+    CHECK_RET(tcpserver.bindSocket(ip, port));
+
+
+    //监听
+    CHECK_RET(tcpserver.listenSocket(5));
+
+
+    //获取新的文件描述符, 如果直接放到循环里面，并且始终只有一个客户端的话
+    //在第二次循环进来时，就会陷入阻塞 
+    TcpSocket newsocket;
+    CHECK_RET(tcpserver.acceptSocket(newsocket));
+
+
+    //进行数据沟通
+    
+    while(1) {
+
+
+        char buf[1024] = {0}; 
+
+        //接收
+        newsocket.receiveSocket(buf, 1023);
+        printf("Client  Say :  %s\n", buf);
+
+
+        //发送
+        printf("Server  Say : ");     
+        fflush(stdout);
+        memset(buf, 0, 1024);
+        scanf("%s", buf);
+        newsocket.sendSocket(buf, strlen(buf));
+        
+    }
+    tcpserver.closeSocket();
+    
+    return 0;
+}
+
